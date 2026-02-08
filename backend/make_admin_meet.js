a20a83056e2127663ecafadadd9b51858c88810c
MeetOp08 <patelmeet52271@gmail.com>
2026-02-08 20:47:20 +0530

    chore: add admin promotion helper script

--- backend/make_admin_meet.js
+++ backend/make_admin_meet.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const path = require('path');
-const connectDB = require('./config/db');
+const bcrypt = require('bcryptjs');
 const User = require('./models/User');
 
-// Load env
 dotenv.config({ path: path.join(__dirname, '.env') });
 
 const makeAdmin = async () => {
@@ -15,17 +14,22 @@ const makeAdmin = async () => {
         console.log("Connected!".green);
 
         const email = "patelmeet5967@gmail.com";
-        const pass = "123456";
+        const pass = "Admin@123"; // NEW PASSWORD
         const user = await User.findOne({ email });
 
         if (user) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(pass, salt);
             user.isAdmin = true;
-            user.password = pass;
+
             await user.save();
-            console.log(`User ${user.name} (${email}) password reset to '${pass}' and is now an Admin! 👑`.green.bold);
+
+            console.log(`User ${user.name} is now ADMIN 👑`.green.bold);
+            console.log(`Login Email: ${email}`.cyan);
+            console.log(`Login Password: ${pass}`.cyan.bold);
         } else {
             console.log(`User with email ${email} not found!`.red);
-            console.log("Please create an account on the website first.".yellow);
+            console.log("Please register on the website first.".yellow);
         }
 
         process.exit();
