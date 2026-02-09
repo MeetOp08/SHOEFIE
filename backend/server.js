4ba61eb2d7a18124349c963d9e8bd7d52735f856
MeetOp08 <patelmeet52271@gmail.com>
2026-02-10 00:29:39 +0530

    update package.json

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ if (environment === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = 5001; // Hardcoded to prevent EADDRINUSE 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
